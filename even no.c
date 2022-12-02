7d0db7b086377bebe6ff74038e859f444415512e
NAZRINTAIBIE <119109208+NAZRINTAIBIE@users.noreply.github.com>
2022-12-02 09:21:29 -0800

    Add files via upload

---
+++ even no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    int n,i;
+    printf("enter any number\n");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("even number");
+      else
+    printf("odd number");
+     return 0;
+}
+
