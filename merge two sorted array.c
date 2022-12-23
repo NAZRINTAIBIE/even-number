4d349942ccdb59914a92bade2b67b3c7fc0a9897
NAZRINTAIBIE <119109208+NAZRINTAIBIE@users.noreply.github.com>
2022-12-23 11:08:12 -0800

    Add files via upload

---
+++ merge two sorted array.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+    int arr1[MAX_SIZE], arr2[MAX_SIZE], mergeArray[MAX_SIZE];
+    int size1, size2, mergeSize;
+    int index1, index2, mergeIndex;
+    int i;
+    
+    printf("Enter the size of 1st array : ");
+    scanf("%d", &size1);
+    
+    printf("Enter elements in 1st array : ");
+    for(i=0; i<size1; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+    
+    printf("\nEnter the size of 2nd array : ");
+    scanf("%d", &size2);
+    
+    printf("Enter elements in 2nd array : ");
+    for(i=0; i<size2; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+    mergeSize = size1 + size2;
+    
+    index1 = 0;
+    index2 = 0;
+    for(mergeIndex=0; mergeIndex < mergeSize; mergeIndex++)
+    {
+        
+        if(index1 >= size1 || index2 >= size2)
+        {
+            break;
+        }
+            if(arr1[index1] < arr2[index2])
+        {
+            mergeArray[mergeIndex] = arr1[index1];
+            index1++;
+        }
+             else
+        {
+            mergeArray[mergeIndex] = arr2[index2];
+            index2++;
+        }
+    }
+    
+    while(index1 < size1)
+    {
+        mergeArray[mergeIndex] = arr1[index1];
+        mergeIndex++;
+        index1++;
+    }
+    while(index2 < size2)
+    {
+        mergeArray[mergeIndex] = arr2[index2];
+        mergeIndex++;
+        index2++;
+    }
+    
+    printf("\nArray merged in ascending order : ");
+        for(i=0; i<mergeSize; i++)
+    {
+        printf("%d\t", mergeArray[i]);
+    }
+        return 0;
+}
+
