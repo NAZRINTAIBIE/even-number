1d1f0664bf5250dd68e049b4b66a513d5b2b15f4
NAZRINTAIBIE <119109208+NAZRINTAIBIE@users.noreply.github.com>
2023-01-14 09:13:31 -0800

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,j,k;
+  char ch;
+
+  printf("Enter number of lines: ");
+  scanf("%d",&n);
+
+  for( i=0; i<=n; i++)
+  {
+    ch = 'A';
+    for( j=0; j<=n-i; j++, ch++)
+    {
+      printf("%c",ch);
+    }
+
+    if(i==0)
+    {
+      printf("%c",ch);
+    }
+    else
+    {
+      for(k=0; k<(2*i)+1; k++){
+        printf(" ");
+      }
+    }
+
+    ch--;
+    for( j=0; j<=n-i; j++, ch--)
+    {
+      printf("%c",ch);
+    }
+
+    printf("\n");
+  }
+  return 0;
+}
+
