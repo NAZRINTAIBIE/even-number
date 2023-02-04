cab2fa4d710cc91cc7aef8b0cdeb20b9d239c0fc
NAZRINTAIBIE <119109208+NAZRINTAIBIE@users.noreply.github.com>
2023-02-04 11:45:03 -0800

    Add files via upload

---
+++ SpyNumber1 .java
@@ -0,0 +1,35 @@
+import java.util.Scanner;  
+public class SpyNumber1 
+{    
+private static boolean isSpyNumber(int number)   
+{  
+int lastDigit = 0;  
+int sum = 0;  
+int product = 1;   
+while(number != 0)   
+{   
+lastDigit = number % 10;  
+sum = sum + lastDigit;    
+product = product * lastDigit;  
+number = number / 10;  
+}  
+if(sum == product)  
+return true;  
+return false;  
+}    
+public static void main(String args[])   
+{  
+int lowerRange = 0, upperRange = 0;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the lower range: ");    
+lowerRange = sc.nextInt();  
+System.out.print("Enter upper range: ");  
+upperRange = sc.nextInt();  
+System.out.println("The Spy numbers between "+ lowerRange + " to "+ upperRange+" are: ");  
+for(int i=lowerRange; i<=upperRange; i++)   
+{    
+if(isSpyNumber(i))    
+System.out.print(i +" ");  
+}  
+}  
+}  
